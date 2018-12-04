29164bc6debd17db759172840245a6b24888cf80
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-12-04 15:27:53 +0100

    Feat: Seed Marker Types

--- GivskudDashboard/Data/ApplicationDataContext.cs
+++ GivskudDashboard/Data/ApplicationDataContext.cs
@@ -20,6 +20,17 @@ namespace GivskudDashboard.Data
 				e.Property(m => m.Lat).HasColumnType("decimal(18, 15)");
 				e.Property(m => m.Lng).HasColumnType("decimal(18, 15)");
 			});
+			modelBuilder.Entity<MarkerType>().HasData(
+					new MarkerType() { ID = 1, Name = "Animal" },
+					new MarkerType() { ID = 2, Name = "Shop" },
+					new MarkerType() { ID = 3, Name = "Accommodation" },
+					new MarkerType() { ID = 4, Name = "Toilet" },
+					new MarkerType() { ID = 5, Name = "Restaurant" },
+					new MarkerType() { ID = 6, Name = "Playground" },
+					new MarkerType() { ID = 7, Name = "Picknick" },
+					new MarkerType() { ID = 8, Name = "Icecram" },
+					new MarkerType() { ID = 9, Name = "Parking" }
+				);
 		}
 
 		public ApplicationDataContext(DbContextOptions options) : base(options)
---
+++ GivskudDashboard/Migrations/20181204142011_MarkerTypesDBSeed.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GivskudDashboard.Migrations
+{
+    public partial class MarkerTypesDBSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MarkerTypes",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Animal" },
+                    { 2, "Shop" },
+                    { 3, "Accommodation" },
+                    { 4, "Toilet" },
+                    { 5, "Restaurant" },
+                    { 6, "Playground" },
+                    { 7, "Picknick" },
+                    { 8, "Icecram" },
+                    { 9, "Parking" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "MarkerTypes",
+                keyColumn: "ID",
+                keyValue: 9);
+        }
+    }
+}
