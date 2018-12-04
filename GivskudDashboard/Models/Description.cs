caa09eef42c8dbad09ed554fcb89fc8aba94dcc8
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-12-04 14:25:50 +0100

    Repair Commiy

--- GivskudDashboard/Models/Description.cs
+++ GivskudDashboard/Models/Description.cs
@@ -12,17 +12,21 @@ namespace GivskudDashboard.Models
 
 		[Required]
 		[MaxLength(64, ErrorMessage = "Title can be maximum 64 characters long")]
+		[Display(Name = "Description Title")]
 		public string Title { get; set; }
 
 		[Required]
 		[MaxLength(255, ErrorMessage = "Title can be maximum 255 characters long")]
+		[Display(Name = "Marker Location")]
 		public string Location { get; set; }
 
 		[DataType(DataType.DateTime)]
+		[Display(Name = "Feeding Time")]
 		public DateTime? FeedingTime { get; set; }
 
 		[Required]
 		[MaxLength(7168, ErrorMessage = "Body can be maximum 7168 characters long")]
+		[Display(Name = "Description Body")]
 		public string Body { get; set; }
 	}
 }
