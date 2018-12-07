49b2180fc22cf1d6fe0b4860dce185117c9b97db
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-12-07 15:58:02 +0100

    Feat: Overview map show markers details

--- GivskudDashboard/Controllers/OverviewController.cs
+++ GivskudDashboard/Controllers/OverviewController.cs
@@ -23,8 +23,38 @@ namespace GivskudDashboard.Controllers
 		public IActionResult Index()
 		{
 			var markers = _context.Markers.ToArray();
-
 			return View(markers);
+		}
+
+		public IActionResult Details(int? id, decimal? lat, decimal? lng)
+		{
+			if (id == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			var marker = _context.Markers
+				.Include(m => m.Description)
+				.SingleOrDefault(m => m.ID == id);
+
+			if (marker == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			var markersList = _context.Markers.ToArray();
+			var types = _context.MarkerTypes.ToDictionary(t => t.ID);
+
+			ViewBag.MarkersList = markersList;
+			ViewBag.Types = types;
+
+			if (lat != null && lat != null)
+			{
+				ViewBag.Lat = lat;
+				ViewBag.Lng = lng;
+			}
+
+			return View(marker);
 		}
     }
 }
