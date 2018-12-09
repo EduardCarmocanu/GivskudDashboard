0cfa30862dc95bc78f2865716a66c27b2c4a503e
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-12-09 13:22:30 +0100

    Revert "Feat: Update links"
    This reverts commit 876a1fcf76e8c83d2d9fb7bf1793bbdd49007a32.

--- GivskudDashboard/Controllers/MarkersController.cs
+++ GivskudDashboard/Controllers/MarkersController.cs
@@ -133,7 +133,7 @@ namespace GivskudDashboard.Controllers
                 return NotFound();
             }
 
-            return RedirectToAction("Index");
+            return View(marker);
         }
 
         // POST: Markers/Delete/5
@@ -144,7 +144,6 @@ namespace GivskudDashboard.Controllers
             var marker = await _context.Markers.FindAsync(id);
             _context.Markers.Remove(marker);
             await _context.SaveChangesAsync();
-
             return RedirectToAction(nameof(Index));
         }
 
