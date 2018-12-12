fa34a5e2d346eb29c5f11eeb5dafdaee21634b8c
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-12-12 13:55:27 +0100

    Feat: Integrate Authentication

---
+++ GivskudDashboard/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GivskudDashboard.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GivskudDashboard.Controllers
+{
+    public class AccountController : Controller
+    {
+		private readonly SignInManager<IdentityUser> _signInManager;
+
+		public AccountController(
+			SignInManager<IdentityUser> signInManager
+		)
+		{
+			_signInManager = signInManager;
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(LoginViewModel login, string returnUrl = null)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(
+					login.EmailAddress, 
+					login.Password, 
+					login.RememberMe, 
+					false
+				);
+
+			if (!result.Succeeded)
+			{
+				ModelState.AddModelError("", "Login Error");
+				return View();
+			}
+
+			if (string.IsNullOrWhiteSpace(returnUrl))
+			{
+				return RedirectToAction("Index", "Markers");
+			}
+
+			return Redirect(returnUrl);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Logout(string returnUrl = null)
+		{
+			await _signInManager.SignOutAsync();
+
+			if (string.IsNullOrWhiteSpace(returnUrl))
+			{
+				return RedirectToAction("Index", "Markers");
+			}
+
+			return Redirect(returnUrl);
+		}
+    }
+}
---
+++ GivskudDashboard/Data/IdentityDbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GivskudDashboard.Data
+{
+	public static class IdentityDbInitializer
+	{
+		public static void SeedUsers(UserManager<IdentityUser> userManger)
+		{
+			if (userManger.FindByEmailAsync("staff@givskud.zoo").Result == null)
+			{
+				IdentityUser user = new IdentityUser
+				{
+					UserName = "staff@givskud.zoo",
+					Email = "staff@givskud.zoo"
+				};
+
+				IdentityResult result = userManger.CreateAsync(user, "Givskud2018!").Result;
+			}
+		}
+	}
+}
